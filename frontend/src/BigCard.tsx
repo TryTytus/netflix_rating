eea226d1dace3b85db3c1c194a6f8b9bb9f1f8a2
adam.tyton <adam.tyton@fingoweb.com>
2026-08-08 00:32:04 +0200

    added animation

--- frontend/src/BigCard.tsx
+++ frontend/src/BigCard.tsx
@@ -2,12 +2,13 @@ import Button from "./Button"
 import type { CardParams } from "./Card"
 
 type BigCardParams = {
+    is_visible: boolean,
     top: () => number,
     left: () => number,
     onMouseLeaveTriggered: () => void
 } & CardParams
 
-function BigCard({title, poster_url, rating, vote_count, top, left, onMouseLeaveTriggered} : BigCardParams)
+function BigCard({title, is_visible, poster_url, rating, vote_count, top, left, onMouseLeaveTriggered} : BigCardParams)
 {
 
     const getFormattedVotes = (vote_count: number): string =>  {
@@ -22,7 +23,8 @@ function BigCard({title, poster_url, rating, vote_count, top, left, onMouseLeave
 
     return (
     <>
-        <div onMouseLeave={() => onMouseLeaveTriggered()} style={{top: top(), left: left()}} className={`w-[250px] rounded-xl overflow-hidden left-0 top-0 shrink-0 absolute z-50 -translate-x-1/2 -translate-y-1/2`}>
+        <div onMouseLeave={() => onMouseLeaveTriggered()} style={{top: top(), left: left()}} 
+        className={`w-[250px] rounded-xl overflow-hidden left-0 top-0 shrink-0 absolute z-50 -translate-x-1/2 -translate-y-1/2 transition-[scale,opacity] ease-linear ${is_visible ? ' scale-100 opacity-100' : ' scale-80 opacity-90'}`}>
             <img className={`h-[300px] w-full object-cover`} src={poster_url} />
             <div className="absolute flex items-center gap-1 bottom-16 bg-gray-700 p-1 rounded-tr-md text-gray-400 text-xs opacity-85">
                 <svg className="h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -25,15 +25,24 @@ function Card({title, poster_url, rating, vote_count}: CardParams) {
     const isHovered: Ref<boolean> = useRef(false)
     const lastTimeChangedMs: Ref<number> = useRef(Number.MAX_VALUE)
     const [isOpened, setIsOpened] = useState(false)
+    const [isVisible, setIsVisible] = useState(false)
+
 
     const openPopupCard = () => {
         setIsOpened(true)
+        requestAnimationFrame(() => {
+            setIsVisible(true)
+        })
     }
 
     const closePopupCard = () => {
         isHovered.current = false
         lastTimeChangedMs.current = Date.now()
-        setIsOpened(false)
+        setIsVisible(false)
+
+            setTimeout(() => {
+                setIsOpened(false)
+            }, 200)
     }
 
     const handleEnter = () => {
@@ -76,7 +85,7 @@ function Card({title, poster_url, rating, vote_count}: CardParams) {
             onMouseLeave={() => handleLeave()}
             className={` shrink-0 w-[200px] relative overflow-y-visible`}
         >
-            {(isOpened) && createPortal((<BigCard onMouseLeaveTriggered={() => closePopupCard()} top={() => getTop()} left={() => getLeft()} {...{title, poster_url, rating, vote_count}} />), document.body)}
+            {(isOpened) && createPortal((<BigCard is_visible={isVisible} onMouseLeaveTriggered={() => closePopupCard()} top={() => getTop()} left={() => getLeft()} {...{title, poster_url, rating, vote_count}} />), document.body)}
             <img className={`h-[250px] w-full object-cover`} src={poster_url} />
             <div className="absolute flex items-center gap-1 bottom-0 bg-gray-700 p-1 rounded-tr-md text-gray-400 text-xs opacity-85">
                 <svg className="h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
